c0335e6f82651d8273224287e68d7fad399f6aba
Chad Eymard <eymardcm@gmail.com>
2024-03-17 21:40:51 -0400

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Chad Eymard on 3/17/24.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
